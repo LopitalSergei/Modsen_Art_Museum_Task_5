4893ec74e3c0d86aada47f0c2b9040151568026c
Laptev Sergei <sl260401@gmail.com>
2024-09-22 20:05:41 +0300

    fix: the paths are set to constants

--- src/components/Routing/index.tsx
+++ src/components/Routing/index.tsx
@@ -1,21 +1,26 @@
-import App from '@components/App';
-import Artwork from '@pages/Artwork';
-import Favorites from '@pages/Favorites';
-import Home from '@pages/Home';
-import NotFound from '@pages/NotFound';
 import { FC } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { routes } from '@constants/routes';
 
 export const Routing: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="/artwork/:id" element={<Artwork />} />
-          <Route path="/favorites" element={<Favorites />} />
+        <Route path={routes.app.path} element={<routes.app.element />}>
+          <Route index element={<routes.home.element />} />
+          <Route
+            path={routes.artWork.path}
+            element={<routes.artWork.element />}
+          />
+          <Route
+            path={routes.favorites.path}
+            element={<routes.favorites.element />}
+          />
         </Route>
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path={routes.notFound.path}
+          element={<routes.notFound.element />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,33 +1,33 @@
-// import App from "@components/App";
-// import Artwork from "@pages/Artwork";
-// import Favorites from "@pages/Favorites";
-// import Home from "@pages/Home";
-// import NotFound from "@pages/NotFound";
+import App from '@components/App';
+import Artwork from '@pages/Artwork';
+import Favorites from '@pages/Favorites';
+import Home from '@pages/Home';
+import NotFound from '@pages/NotFound';
 import home from '@assets/icons/home.svg';
 import bookmark from '@assets/icons/bookmarkIcon.svg';
 
-// export const routes = [
-//   {
-//     path: "/",
-//     element: App,
-//   },
-//   {
-//     path: "/home",
-//     element: Home,
-//   },
-//   {
-//     path: "/favorites",
-//     element: Favorites,
-//   },
-//   {
-//     path: "/artwork",
-//     element: Artwork,
-//   },
-//   {
-//     path: "*",
-//     element: NotFound,
-//   },
-// ];
+export const routes = {
+  app: {
+    path: '/',
+    element: App,
+  },
+  home: {
+    path: '/home',
+    element: Home,
+  },
+  favorites: {
+    path: '/favorites',
+    element: Favorites,
+  },
+  artWork: {
+    path: '/artwork/:id',
+    element: Artwork,
+  },
+  notFound: {
+    path: '*',
+    element: NotFound,
+  },
+};
 
 export const navLinks = [
   {
